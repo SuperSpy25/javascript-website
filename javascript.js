247f65be235ac593bed6022b82def96813c93168
Caleb <jenkorbjanke@gmail.com>
2023-11-02 13:45:49 -0400

    working button

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
-function onclick(){
-    document.getElementById("output").innerHTML = "hewo"
+function onClick(){
+
+        document.getElementById("output").innerHTML = "why did you press me";
+        const myVaribleName = {name: "hahhhahahhahhahahhahaahahhhaahhahhahahahahahahahahhahahahahhahahahahahahahahahahhaha"};
+        console.log(myVaribleName.name);
+        const myList = [];
+        console.log(myList[0])
 }
+
