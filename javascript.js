8c19c7419a52b47e1b7e72241565f69fc950a8b6
Caleb <jenkorbjanke@gmail.com>
2023-11-21 13:27:43 -0500

    hiiiii

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
-function onClick(InputName){
+function onClick(InputName) {
 
         document.getElementById("output").innerHTML = "cool huh";
-        alert('Hello, ' + InputName + '!');
+        const user = {name: 'Bob'};
+        console.log('Hello, ' + user.name);
+
+        const names = ['Sally', 'Jerome'];
+        const users =[{name: 'Sally'}{name: 'Jerome'}];
+        console.log(' First name in list -----', names[0]);
+
+        console.log('Second name in list of objects -----',users[1].name);
+        alert('Hello, ' + InputName + '!')
+  
 }
 
