848fcf9be1db9d04d8f78593e6938a85cf7542cf
Caleb <jenkorbjanke@gmail.com>
2023-12-05 13:15:25 -0500

    hi

--- javascript.js
+++ javascript.js
@@ -1,15 +1,21 @@
 function onClick(InputName) {
 
-        document.getElementById("output").innerHTML = "cool huh";
         const user = {name: 'Bob'};
         console.log('Hello, ' + user.name);
 
         const names = ['Sally', 'Jerome'];
-        const users =[{name: 'Sally'}{name: 'Jerome'}];
+        const users =[{name: 'Sally'},{name: 'Jerome'}];
         console.log(' First name in list -----', names[0]);
 
         console.log('Second name in list of objects -----',users[1].name);
-        alert('Hello, ' + InputName + '!')
+        const url = 'https://pokeapi.co/api/v2/pokemon';
+        $.get(url,(data,status)=>{
+                if (status ==='success') {
+                        console.log(data);
+                        const pokemonList = data.results
+                        document.getElementById("output").innerHTML = pokemonList;
+                }
+        });
   
 }
 
