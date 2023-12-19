0e95a9ce29d4a5adff53afc938796ddf74c8453e
Caleb <jenkorbjanke@gmail.com>
2023-12-19 13:38:09 -0500

    poooooooooooooooooooooooooooooo

--- javascript.js
+++ javascript.js
@@ -1,22 +1,16 @@
 function onClick(InputName) {
-
-        const url = 'https://pokeapi.co/api/v2/pokemon';
-        $.get(url,(data,status)=>{
-                if (status ==='success') {
-                        console.log(data);
-                        const pokemonList = data.results.map(pokemon => {
-                             const h2Tag = document.createElement('h2');
-                             console.log('Name with H2 tag: ', h2Tag)
-                             document.getElementById("output").innerHTML = pokemonList.append(h2Tag)
-                                return h2Tag
-                        });
-                        document.getElementById("output").innerHTML = pokemonList;
-                       
-                } else {
-                        console.log('Status was: ', status);
-                }
-                        
-        });
-  
+  const url = "https://pokeapi.co/api/v2/pokemon";
+  $.get(url, (data, status) => {
+    if (status === "success") {
+      console.log(data);
+      data.results.forEach((pokemon) => {
+        const h2Tag = document.createElement("h2");
+        console.log("Name with H2 tag: ", h2Tag);
+        h2Tag.innerHTML = pokemon.name;
+        document.getElementById("output").append(h2Tag);
+      });
+    } else {
+      console.log("Status was: ", status);
+    }
+  });
 }
-
