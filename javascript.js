8cd84c7d9b4d07c985efcee2ff611f509d22319b
Caleb <jenkorbjanke@gmail.com>
2024-01-08 11:33:25 -0500

    homework done

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
 function onClick(InputName) {
   const url = "https://pokeapi.co/api/v2/pokemon";
   $.get(url, (data, status) => {
-    if (status === "success") {
-      console.log(data);
-      data.results.forEach((pokemon) => {
-        const h2Tag = document.createElement("h2");
-        console.log("Name with H2 tag: ", h2Tag);
-        h2Tag.innerHTML = pokemon.name;
-        document.getElementById("output").append(h2Tag);
-      });
-    } else {
-      console.log("Status was: ", status);
-    }
-  });
+      if (status === "success") {
+        console.log(data);
+        data.results.forEach((pokemon) => {
+          const h2Tag = document.createElement("h2");
+          console.log("Name with H2 tag: ", h2Tag);
+          h2Tag.innerHTML = pokemon.name;
+          document.getElementById("output").append(h2Tag);
+        });
+      } else {
+        console.log("Status was: ", status);
+      }
+    });
 }
