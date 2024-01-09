ec829fafee43c1363d2150b2ebe016bbd24befd4
Caleb <jenkorbjanke@gmail.com>
2024-01-09 13:47:39 -0500

    bug fixed

--- javascript.js
+++ javascript.js
@@ -1,16 +1,20 @@
 function onClick(InputName) {
   const url = "https://pokeapi.co/api/v2/pokemon";
   $.get(url, (data, status) => {
-      if (status === "success") {
-        console.log(data);
-        data.results.forEach((pokemon) => {
-          const h2Tag = document.createElement("h2");
-          console.log("Name with H2 tag: ", h2Tag);
-          h2Tag.innerHTML = pokemon.name;
-          document.getElementById("output").append(h2Tag);
-        });
-      } else {
-        console.log("Status was: ", status);
-      }
-    });
+    if (status === "success") {
+      addNamesToHTML(data);
+    } else {
+      console.log("Status was: ", status);
+    }
+  });
+}
+function addNamesToHTML(data) {
+  console.log(data);
+  const pokemonNames = data.results.map((pokemon) => {
+    const h2Tag = document.createElement("h2");
+    console.log("Name with H2 tag: ", h2Tag);
+    h2Tag.innerHTML = pokemon.name;
+    return h2Tag;
+  });
+  $("#output").html(pokemonNames);
 }
